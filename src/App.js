c2125f1589e5a64a8c08ff3a868bc2eb321cda95
IlyasHussain07 <hilyas904@gmail.com>
2021-10-28 17:42:42 +0530

    Sorting everything in a correct way just follow the folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            {/* Add Header , Main , Footer */}
+            Hello Everyone !!
+          </Route>
+          <Route path='/mens'>
+            {/* Add Header ,Men Products and Footer */}
+          </Route>
+          <Route path='/womens'>
+            {/* Add Header, Women Products and Footer */}
+          </Route>
+          <Route path='/shoes'>
+            {/* Add Header, Shoes Products and Footer */}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
